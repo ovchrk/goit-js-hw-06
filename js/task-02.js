bfe148e06094df19f52fd0c939c873cec448197d
ovchrk <113932934+ovchrk@users.noreply.github.com>
2023-02-13 18:31:41 +0200

    task2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,25 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector("#ingredients");
+const list = ingredients.map(ingredient => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = ingredient;
+  return liEl;
+  console.log(liEl);
+});
+listEl.append(...list);
+
+
+
+// const list = ingredients.forEach(ingredient => {
+//   const liEl = document.createElement("li");
+//   liEl.classList.add("item");
+//   liEl.textContent = ingredient;
+
+//   console.log(liEl);
+
+//   // listEl.append(liEl);
+// });
