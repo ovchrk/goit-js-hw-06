54ab00dc591b819474d660ca6c48abbae7427014
ovchrk <113932934+ovchrk@users.noreply.github.com>
2023-02-14 14:40:51 +0200

    task8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener('submit', formSubmitHandler);
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+    const formEmailValue = event.currentTarget.elements.email.value;
+    const formPasswordValue = event.currentTarget.elements.password.value;
+    
+    if (formEmailValue === "" || formPasswordValue === "") {
+        alert(`Please, fill in all fields!`);
+    }
+
+    // console.log(event.currentTarget.elements);
+    // console.log(event.currentTarget.elements.email.value);
+    // console.log(event.currentTarget.elements.password.value);
+
+};
